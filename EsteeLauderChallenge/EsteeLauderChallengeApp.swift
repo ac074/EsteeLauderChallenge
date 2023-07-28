3913e39797fd1af0e51e92a942a6cbf564dde719
Amulya <amu7214@gmail.com>
2023-07-27 20:11:44 -0400

    Initial Commit

---
+++ EsteeLauderChallenge/EsteeLauderChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  EsteeLauderChallengeApp.swift
+//  EsteeLauderChallenge
+//
+//  Created by Amulya on 7/27/23.
+//
+
+import SwiftUI
+
+@main
+struct EsteeLauderChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
